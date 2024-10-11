7a57de1c1e03d2e30d08a9181a0fa82e5340258f
Jhonny Kyuorhan <jk.developrs@gmail.com>
2024-10-11 17:15:17 -0400

    Export all theme colors and fonts.

--- src/theme/fonts.ts
+++ src/theme/fonts.ts
@@ -1,25 +1,30 @@
-export const fonts = {
-  family: {
-    regular: {
-      inter: "Inter_400Regular",
-      poppins: "Poppins_400Regular",
-    },
-    medium: {
-      inter: "Inter_500Medium",
-      poppins: "Poppins_500Medium",
-    },
-    semibold: {
-      inter: "Inter_600SemiBold",
-      poppins: "Poppins_600SemiBold",
-    },
-    bold: {
-      inter: "Inter_700Bold",
-      poppins: "Poppins_700Bold",
-    },
-  },
-  size: {
-		sm: 12,
-		md: 14,
-		lg: 16,
-  },
-}
+import { Dimensions } from "react-native";
+
+const { height } = Dimensions.get("window");
+
+export const fontFamily = {
+  Inter_900Black: require("../../assets/fonts/Inter_900Black.ttf"),
+  Inter_800ExtraBold: require("../../assets/fonts/Inter_800ExtraBold.ttf"),
+  Inter_700Bold: require("../../assets/fonts/Inter_700Bold.ttf"),
+  Inter_600SemiBold: require("../../assets/fonts/Inter_600SemiBold.ttf"),
+  Inter_500Medium: require("../../assets/fonts/Inter_500Medium.ttf"),
+  Inter_400Regular: require("../../assets/fonts/Inter_400Regular.ttf"),
+  Poppins_700Bold: require("../../assets/fonts/Poppins_700Bold.ttf"),
+  Poppins_600SemiBold: require("../../assets/fonts/Poppins_600SemiBold.ttf"),
+  Poppins_500Medium: require("../../assets/fonts/Poppins_500Medium.ttf"),
+  Poppins_400Regular: require("../../assets/fonts/Poppins_400Regular.ttf"),
+} as const;
+
+export const fontSize = {
+  xs: height * 0.012,
+  sm: height * 0.014,
+  lg: height * 0.016,
+  xl: height * 0.018,
+  xl2: height * 0.02,
+  xl2_1: height * 0.022,
+  xl3: height * 0.024,
+  xl3_1: height * 0.026,
+  xl4: height * 0.03,
+  xl4_1: height * 0.032,
+  xl5: height * 0.036,
+};
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,6 +1,2 @@
-import { colors } from "./colors";
-import { fonts } from "./fonts";
-
-const theme = { colors, fonts };
-
-export default theme;
+export * from "./colors";
+export * from "./fonts";
